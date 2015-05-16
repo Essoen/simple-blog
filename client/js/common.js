ca83e3863e58d46e464115b538283d768206b943
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-05-16 11:40:58 +0200

    Move routes and db-declaration to common lib-directory

--- client/js/common.js
+++ client/js/common.js
@@ -2,7 +2,5 @@
  * Created by esso on 15.05.15.
  */
 
-Blogposts = new Mongo.Collection("Blogposts");
-
 Session.set("availableRoles", ['admin', 'author']);
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,3 @@
-Blogposts = new Mongo.Collection("Blogposts");
-
-
 Meteor.startup(function () {
   // code to run on server at startup
 
